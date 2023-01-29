dedead7f4714ae13c5fbf6695fb4b3c2d7dcacdf
Chima <orlandofays3@gmail.com>
2023-01-30 00:46:50 +0100

    fix credentials

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,8 @@ const task = cron.schedule(
         }
     });
 
-    task.start();
+// task.start();
+tweet();
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}...`);
