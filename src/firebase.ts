3ed82d244dcdde89515887544f5f0ed92a4546ec
Chima <orlandofays3@gmail.com>
2023-02-01 17:22:20 +0100

    fix: tsconfig

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,5 @@
 import admin from "firebase-admin";
-import firebaseConfigJson from "../.firebase/firebaseConfig";
+import firebaseConfigJson from "../firebaseConfig";
 import { getDatabase } from 'firebase-admin/database';
 import { ITopic } from "./utility";
 
